69b3d11e5817bf1101d1dc8fc8cd3bbe284a4d43
scorpiocat <tianxiemaochiyu@outlook.com>
2021-01-21 14:13:23 +0800

    anchor points

--- src/config/index.js
+++ src/config/index.js
@@ -11,8 +11,12 @@ export const HTTP_DEFAULT_CONFIG = {
 
 // 路由默认配置，路由表并不从此注入
 export const ROUTER_DEFAULT_CONFIG = {
-    scrollBehavior() {
-        return { x: 0, y: 0 };
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { x: 0, y: 0 };
+        }
     },
     mode: "history",
     base: "",
