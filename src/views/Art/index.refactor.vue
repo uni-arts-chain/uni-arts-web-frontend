8b147d88875906ea3c6fe5fc143197bd0c16a271
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-04-15 14:10:47 +0800

    add new compoenet

---
+++ src/views/Art/index.refactor.vue
@@ -0,0 +1,13 @@
+/** * Created by Lay Hunt on 2021-04-15 12:10:30. */
+<template>
+    <div class="index"></div>
+</template>
+<script>
+export default {
+    name: "index",
+    data() {
+        return {};
+    },
+};
+</script>
+<style lang="scss" scoped></style>
